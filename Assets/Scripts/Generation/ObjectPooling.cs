2b0f543dea2cbe4a057fc834b430f061a9c73615
klendigocci <klendigocci@gmail.com>
2017-07-07 20:57:10 +0200

    :zap: Fixed Folders And Working on AutoGeneration

---
+++ Assets/Scripts/Generation/ObjectPooling.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectPooling : MonoBehaviour
+{
+    public GameObject pooledObject;
+
+    public int poolAmount;
+
+    List<GameObject> pooledObjects;
+
+    private void Start()
+    {
+        pooledObjects = new List<GameObject>();
+
+        for (int i = 0; i < poolAmount; i++)
+        {
+            GameObject obj = Instantiate(pooledObject) as GameObject;
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
+        }
+    }
+
+    public GameObject GetPoolObject()
+    {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+
+        GameObject obj = Instantiate(pooledObject) as GameObject;
+        obj.SetActive(false);
+        pooledObjects.Add(obj);
+
+        return obj;
+    }
+}
---
+++ Assets/Scripts/Generation/PlatformDestroyer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformDestroyer : MonoBehaviour
+{
+    public GameObject thePlatformDestructionPoint;
+
+
+    private void Start()
+    {
+        thePlatformDestructionPoint = GameObject.Find("PlatformDestroyerPoint");
+    }
+
+    private void Update()
+    {
+        if(transform.position.x < thePlatformDestructionPoint.transform.position.x)
+        {
+            //Destroy(gameObject);
+            gameObject.SetActive(false);
+        }
+    }
+}
