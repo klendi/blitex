55214ed0708b956c178ae981ace74c7f6e8e8f37
klendigocci <klendigocci@gmail.com>
2017-07-25 16:13:15 +0200

    :zap: Fixed snow not saving, replaced lot of balls

--- Assets/Scripts/Level/LevelCompleted.cs
+++ Assets/Scripts/Level/LevelCompleted.cs
@@ -4,22 +4,27 @@ using UnityEngine.UI;
 public enum LevelType
 {
     NormalLevels,
-    SnowLevels
+    SnowLevels,
+    LevelText
 }
 public class LevelCompleted : MonoBehaviour 
 {
     Text tx;
     public LevelType levelType = LevelType.NormalLevels;
+
     private void Awake()
     {
         tx = GetComponent<Text>();
-    }
-    private void Update () 
-	{
+
         if (levelType == LevelType.NormalLevels)
             tx.text = string.Format("YOU COMPLETED LEVEL {0}", Manager.Instance.sceneIndex);
 
         else if (levelType == LevelType.SnowLevels)
             tx.text = string.Format("YOU COMPLETED LEVEL {0}", Manager.Instance.sceneIndex - 15);
+
+        else if(levelType == LevelType.LevelText)
+        {
+            gameObject.GetComponent<TextMesh>().text = string.Format("LEVEL {0}", Manager.Instance.sceneIndex);
+        }
 	}
 }
